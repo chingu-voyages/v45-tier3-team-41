bd1b24ebccad9ec1f41ce270e7fcd6b9842fdf05
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-08-29 17:14:30 +0100

    Add components to SignUp Form and Page

--- app/(auth)/sign-up/page.js
+++ app/(auth)/sign-up/page.js
@@ -1,13 +1,32 @@
+import SignUp from "@/components/forms/SignUp";
+import Image from "next/image";
+
 export default function page() {
   return (
-    <main>
-      {/*  Add Logo App */}
+    <main className="max-w-lg mx-auto">
+      <div className="bg-dark-2 rounded-xl shadow-lg px-10 py-14 space-y-7 border border-dark-3">
+        {/* Logo App */}
+        <Image
+          className="mx-auto"
+          src="/assets/logo.svg"
+          alt="logo"
+          width={40}
+          height={40}
+        />
 
-      {/* Add Title */}
+        {/* Title */}
+        <h1 className="head-text text-center">Sign Up</h1>
 
-      {/* Add description */}
+        {/* Description */}
+        <p className="text-base-regular text-light-2 text-center">
+          Hey, please fill the form to create your Movie Matcher account
+        </p>
 
-      {/* Call the signUp form (Don't forget to import that on the top) */}
+        {/* Calling the Sign Up Form */}
+        <div className="mt-10">
+          <SignUp />
+        </div>
+      </div>
     </main>
   );
 }
