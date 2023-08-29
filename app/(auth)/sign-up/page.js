6c7452d8b82a3bef12c2affeee69e16ee3c40e96
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-08-29 15:27:35 +0100

    Add components to SignUp Page.

--- app/(auth)/sign-up/page.js
+++ app/(auth)/sign-up/page.js
@@ -1,13 +1,47 @@
+// export default function page() {
+//   return (
+//     <main>
+//       {/*  Add Logo App */}
+
+//       {/* Add Title */}
+
+//       {/* Add description */}
+
+//       {/* Call the signUp form (Don't forget to import that on the top) */}
+//     </main>
+//   );
+// }
+
+
+import SignUp from "@/components/forms/SignUp";
+import Image from "next/image";
+
 export default function page() {
   return (
-    <main>
-      {/*  Add Logo App */}
+    <main className="max-w-lg mx-auto">
+      <div className="bg-dark-2 rounded-xl shadow-lg px-10 py-14 space-y-7 border border-dark-3">
+        {/* Logo App */}
+        <Image
+          className="mx-auto"
+          src="/assets/logo.svg"
+          alt="logo"
+          width={40}
+          height={40}
+        />
 
-      {/* Add Title */}
+        {/* Title */}
+        <h1 className="head-text text-center">Sign Up</h1>
 
-      {/* Add description */}
+        {/* Description */}
+        <p className="text-base-regular text-light-2 text-center">
+          Hey, please fill the form to create your Movie Matcher account
+        </p>
 
-      {/* Call the signUp form (Don't forget to import that on the top) */}
+        {/* Calling the Sign In Form */}
+        <div className="mt-10">
+          <SignUp />
+        </div>
+      </div>
     </main>
   );
 }
--- components/forms/SignUp.js
+++ components/forms/SignUp.js
@@ -1,18 +1,123 @@
+// import React from "react";
+// // Don't forget to import those components like in the forms/SignIn file
+// export default function SignUp() {
+//   return (
+//     <div>
+//       {/* Calling the shadcn form */}
+
+//       {/* Add username Field */}
+//       {/* Add Email Field */}
+//       {/* Add password Field */}
+//       {/* Add Confirm password Field */}
+
+//       {/* Add the submitting button */}
+
+//       {/* Add text link to back to sign in form */}
+//     </div>
+//   );
+// }
+
+"use client";
+
 import React from "react";
-// Don't forget to import those components like in the forms/SignIn file
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+
 export default function SignUp() {
+  const form = useForm();
+
+  const onSubmit = () => {};
+
   return (
     <div>
       {/* Calling the shadcn form */}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          {/* Start username Field */}
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="account-form_input no-focus"
+                    placeholder="Enter your username"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* End username Field */}
 
-      {/* Add username Field */}
-      {/* Add Email Field */}
-      {/* Add password Field */}
-      {/* Add Confirm password Field */}
+          {/* Start Password Field */}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="password"
+                    className="account-form_input no-focus"
+                    placeholder="Enter your password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* End Password Field */}
 
-      {/* Add the submitting button */}
+          {/* Start Confirm Password Field */}
+          <FormField
+            control={form.control}
+            name="confirm-password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="password"
+                    className="account-form_input no-focus"
+                    placeholder="Confirm your password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* End Password Field */}
 
-      {/* Add text link to back to sign in form */}
+
+          {/* Submitting Button For the Form */}
+          <Button className="bg-primary-500 w-full py-6" type="submit">
+            Sign Up
+          </Button>
+
+          {/* Text that ask you if you to sign up */}
+          <p className="text-light-1 text-center">
+            Do you have an account?{" "}
+            <Link className="text-primary-500 font-medium" href="/sign-in">
+              Sign In
+            </Link>
+          </p>
+        </form>
+      </Form>
     </div>
   );
 }
